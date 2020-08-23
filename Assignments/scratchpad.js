dacb1a181e52f314fe5639d458ea80f17452f93e
Rohil Khakhar <38146225+rohilreddevil@users.noreply.github.com>
2020-08-22 20:17:11 -0400

    Add files via upload

---
+++ Assignments/scratchpad.js
@@ -0,0 +1,33 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+/*****************************
+*Task 1
+*****************************/
+
+var studentname= "Rohil Khakhar";
+var no_of_courses=5;
+var course_name= "Computer Programming & Analysis";
+var yes= "have";//updated
+var no= "don't have"; //updated
+
+//updated output is as follows after the introduction of two new variables// 
+console.log("My name is " +studentname+ " and I'm in " +course_name+ " program."+" I'm taking " +no_of_courses+ " courses in this semester and I "+no+" a part-time job now."); 
+
+/*****************************
+*Task 2
+*****************************/
+
+var current_year= 2018;
+var age= prompt("Please enter your age:");
+var birth_year= current_year-age;
+console.log("You were born in the year of "+birth_year+ ".");
+var college_years= prompt("Enter the number of years you expect to study in the college:");
+var graduate_year= 2020;
+console.log("You will graduate from Seneca college in the year of " +graduate_year+ ".");
---
+++ Assignments/scratchpad2.js
@@ -0,0 +1,24 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+var x=typeof(123);
+console.log(x);
+
+var words= "Rohil"+" Khakhar";
+console.log(words);
+
+var value=5;
+var combination=value + "4";
+console.log(combination);
+
+var number = prompt("Enter a number: ");
+console.log(number);
+var value=5;
+var combination=(value + "4")*number
+console.log(combination);
